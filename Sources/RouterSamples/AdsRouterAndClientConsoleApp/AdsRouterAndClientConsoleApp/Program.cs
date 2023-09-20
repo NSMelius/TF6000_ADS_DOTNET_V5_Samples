9d0e50aaa2bcaf261ffa89ef1b545c169853d6fe
Nathan S Melius <NSmelius@gmail.com>
2023-09-20 16:02:58 -0500

    Made changes to Base Samples to improve execution of Sample 80 Reactive: Functions now correctly request the symbol info of the first task's cycle count. Added new variables to the PLC projects GVL to be read by the functions SubscribeReactiveValueChanges() and SubscribeReactiveSymbolChanges where there were none that fit the search criteria Changed the target AmsNetId of the client used in SubscribeReactiveValueChanges to target the local Net Id instead of a string constant.

--- Sources/RouterSamples/AdsRouterAndClientConsoleApp/AdsRouterAndClientConsoleApp/Program.cs
+++ Sources/RouterSamples/AdsRouterAndClientConsoleApp/AdsRouterAndClientConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -6,9 +7,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using TwinCAT.Ads;
-using TwinCAT.Ads.SystemService;
 using TwinCAT.Ads.TcpRouter;
-using TwinCAT.Router;
 
 namespace AdsRouterAndClientConsoleApp
 {
@@ -18,57 +17,32 @@ namespace AdsRouterAndClientConsoleApp
     {
         // Application settings as Static Variables here
         static AmsNetId _localNetId = new AmsNetId("1.1.1.1.1.1");
-        
-        static AmsNetId _remoteNetId = new AmsNetId("3.3.3.3.1.1");
-        static string _remoteRouteName = "CodedRemote";
-        static IPAddress _remoteIp = IPAddress.Parse("192.168.0.3");
-
-        static ILogger? _logger = null;
+        static AmsNetId _remoteNetId = new AmsNetId("2.2.2.2.1.1");
+        static string _remoteRouteName = "Remote1";
+        static IPAddress _remoteIp = IPAddress.Parse("192.168.0.2");
 
         private async static Task Main(string[] args)
         {
 
             try
             {
-                using var cancelSource = new CancellationTokenSource();
-                var cancel = cancelSource.Token;
-
-                using var routerCancelSource = new CancellationTokenSource();
-                var routerCancel = routerCancelSource.Token;
+                using var cancelTokenSource = new CancellationTokenSource();
+                var cancelToken = cancelTokenSource.Token;
 
                 using var loggerFactory = LoggerFactory.Create(builder =>
                 {
-                    builder.SetMinimumLevel(LogLevel.Information);
+                    builder.SetMinimumLevel(LogLevel.Trace);
                     builder.AddConsole();
                 });
 
-                _logger = loggerFactory.CreateLogger("AdsRouter");
-
                 Console.WriteLine("Starting Router");
-                var router = new AmsTcpIpRouter(_localNetId, _logger);
-
-                //Use this overload to instantiate a Router without support of IHost / IConfigurationProvider support and parametrize by code
-                //var x = new AmsTcpIpRouter(loggerFactory.CreateLogger("AdsRouter"), _configuration);
-
-                //Apart from using AppSettings configuration, Routes can be added also by code:
+                var router = new AmsTcpIpRouter(_localNetId, loggerFactory.CreateLogger("AdsRouter"));
                 router.AddRoute(new Route(_remoteRouteName, _remoteNetId, new IPAddress[] { _remoteIp }));
 
                 // Starts asynchronously without awaiting!!!
                 // So that the router is runnning in a Worker Task in parallel to the Console
                 // main thread!
-                using Task routerTask = router.StartAsync(routerCancel); // Start the router
-
-                // Starting included AdsServers
-                // In this case we add the simple TwinCAT Router (AmsPort 1) to support adding and removing routes
-                // (Add-AdsRoute, Remove-AdsRoute from TcXaeMgmt powershell module are supported)
-                AdsRouterServer adsRouterService = new AdsRouterServer(router, _logger);
-
-                // And a simple TwinCAT System Service (AmsPort 10000) for supporting browsing routes
-                // (Get-AdsRoute support including BroadcastSearch 'Get-AdsRoute -all' in 'TcXaeMgmt' Powershell module)
-                SystemServiceServer systemService = new SystemServiceServer(router, _logger);
-
-                Task systemServiceTask = systemService.ConnectServerAndWaitAsync(cancel);
-                Task routerServerTask = adsRouterService.ConnectServerAndWaitAsync(cancel);
+                using var routerTask = router.StartAsync(cancelToken);
 
                 while (!router.IsRunning)
                 {
@@ -80,34 +54,25 @@ namespace AdsRouterAndClientConsoleApp
                 using var client = new AdsClient(loggerFactory.CreateLogger("AdsClient"));
                 client.Connect(new AmsAddress(_remoteNetId, 10000));
 
+
                 Console.WriteLine("Client connected");
 
                 bool stop = false;
                 do
                 {
                     // Read State asynchronously and await
-                    ResultReadDeviceState result = await client.ReadStateAsync(cancel);
+                    ResultReadDeviceState result = await client.ReadStateAsync(cancelToken);
+
                     Console.WriteLine($"State: {result.State.AdsState}");
 
                     Console.WriteLine("Press Enter to read state, 'c' + <Enter> for leave ...");
-                    string? line = Console.ReadLine();
+                    string line = Console.ReadLine();
                     if (line == "c" || line == "C")
                         stop = true;
                 } while (!stop);
 
-                // Cancel AdsServers
-                cancelSource.Cancel();
-
-                // Wait for disconnecting all AdsServers
-                await Task.WhenAll(systemServiceTask, routerServerTask);
-
-                // Stops the Router
-                router.Stop();
-
-                // And wait for the task to finish
-                await routerTask;
-                Console.WriteLine("Succeeded!");
-
+                // Cancel all running tasks
+                cancelTokenSource.Cancel();
             }
             catch (Exception ex)
             {
@@ -133,7 +98,7 @@ namespace AdsRouterAndClientConsoleApp
                 {
                     // Add further AppConfigurationProvider here.
                     config.Sources.Clear(); // Clear all default config sources 
-                    //config.AddJsonFile("appSettings.json"); // Use Appsettings Configuration
+                    config.AddJsonFile("appSettings.json"); // Use Appsettings Configuration
                     //config.AddEnvironmentVariables("ENV_"); // Use Environment variables
                     //config.AddCommandLine(args); // Use Command Line
                     //config.AddStaticRoutesXmlConfiguration(); // Overriding settings with StaticRoutes.Xml 
@@ -142,7 +107,7 @@ namespace AdsRouterAndClientConsoleApp
                 {
                     logging.ClearProviders();
                     // Adding console logging here.
-                    //logging.AddConsole();
+                    logging.AddConsole();
                 })
                 .Build()
                 .Run();
